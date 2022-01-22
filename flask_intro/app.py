8d1ad67b9c99a73c82b24cc3f0719ef2dcc4d74e
sodonova <54714602+sodonova@users.noreply.github.com>
2022-01-22 13:37:06 -0500

    Add ical submission page

--- flask_intro/app.py
+++ flask_intro/app.py
@@ -1,11 +1,25 @@
 # import the Flask class from the flask module
-from flask import Flask, render_template, redirect, url_for, request, session, flash
+# from crypt import methods
+from flask import Flask, render_template, redirect, url_for, request, session
 from flask_sqlalchemy import SQLAlchemy
+from functools import wraps
+
 # create the application object
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.secret_key = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ' # this is an arbitrary string
 db = SQLAlchemy(app)
+
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args,**kwargs)
+        else:
+            # flash stuff we don't care about
+            return redirect(url_for('login'))
+    return wrap
+
 
 # class User(db.Model):
 #     __tablename__ = "users"
@@ -24,20 +38,39 @@ def home():
 def login():
     error = None
     if request.method == 'POST':
-        if not verify_login():
+        if not verify_login(request.form['username'], request.form['password']):
             error = 'Invalid Credentials. Please try again.'
         else:
             session['logged_in'] = True
             return redirect(url_for('home'))
     return render_template('login.html', error=error)
 
-def verify_login():
+def verify_login(username, password):
     return True
 
 @app.route('/logout')
 def logout():
     session.pop('logged_in',None)
     return redirect(url_for('home'))
+
+@app.route('/ical', methods=['GET','POST'])
+@login_required # make sure user is authed
+def get_ical():
+    error=None
+    if request.method == 'GET':
+        # just display template
+        return render_template('ical.html',error=error)
+    elif request.method == 'POST':
+        # get user's ical link
+        link = request.form['icalurl']
+        print(link)
+        # imaginary_ical_func(link)
+        # store users free time (presumably sql)
+        # when they click submit, we want to redirect them to a confirmation page that gives them the group chat
+
+@app.route('/confirm')
+def confirm():
+    pass
 
 @app.route('/welcome')
 def welcome():
@@ -46,3 +79,5 @@ def welcome():
 # start the server with the 'run()' method
 if __name__ == '__main__':
     app.run(debug=True)
+
+
