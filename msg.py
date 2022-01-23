876fda5141b123c6d7f9dbe6e1313d423d972cef
sodonova <54714602+sodonova@users.noreply.github.com>
2022-01-23 01:50:34 -0500

    Send message easy

--- msg.py
+++ msg.py
@@ -2,7 +2,7 @@ import os
 from pydoc import cli
 from twilio.rest import Client
 
-phonenum = [ "6147023950", "3172497574","9148067766", "7658389656"]
+phonenum = ["3172497574","9148067766", "7658389656"] # "6147023950"
 
 
 # Your Account SID from twilio.com/console
@@ -13,28 +13,28 @@ auth_token  = "1c35356d92d60326c491a2d896aa30c5"
 client = Client(account_sid, auth_token)
 
 
-conversation = client.conversations \
-                     .conversations \
-                     .create(friendly_name='Study group')
-message = client.conversations \
-    .conversations(conversation.sid) \
-    .participants \
-                    .create(
-                         identity='twilio',
-                         messaging_binding_projected_address='+16073005429'
-                     )
+# conversation = client.conversations \
+#                      .conversations \
+#                      .create(friendly_name='Study group')
+# message = client.conversations \
+#     .conversations(conversation.sid) \
+#     .participants \
+#                     .create(
+#                          identity='twilio',
+#                          messaging_binding_projected_address='+16073005429'
+#                      )
 
-for x in phonenum:
-    client.conversations \
-                    .conversations(conversation.sid) \
-                    .participants \
-                    .create(messaging_binding_address='+1'+x)
+#for x in phonenum:
+ #   client.conversations \
+  #                  .conversations('CHaca7ccf263504d7589898b02eee805b5') \
+   #                 .participants \
+    #                .create(messaging_binding_address='+1'+x)
 
 message = client.conversations \
-    .conversations(conversation.sid) \
+    .conversations('CHaca7ccf263504d7589898b02eee805b5') \
     .messages \
     .create(
-         body='This is the class chat!',
+         body='This is the class  chat!',
          author='twilio'
      )
 
